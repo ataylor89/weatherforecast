f3092d2d0c4fa3726b3144c7dfd317980c75ca5e
Andrew Taylor <rolyata47@gmail.com>
2023-04-11 06:53:46 -0500

    updating the readme

--- weatherforecast/__init__.py
+++ weatherforecast/__init__.py
@@ -1,24 +1,9 @@
 from flask import Flask
 import configparser
-import logging
 
 app = Flask(__name__)
-
 config = configparser.ConfigParser()
 config.read('weather.ini')
 bing_api_key = config.get('DEFAULT', 'BING_API_KEY', fallback=None)
-
-logger = logging.getLogger()
-logger.setLevel(logging.INFO)
-ch = logging.StreamHandler()
-ch.setLevel(logging.INFO)
-fh = logging.FileHandler('weatherforecast.log')
-fh.setLevel(logging.INFO)
-formatter = logging.Formatter("%(asctime)s;%(module)s;%(funcName)s;%(levelname)s;%(message)s")
-ch.setFormatter(formatter)
-fh.setFormatter(formatter)
-logger.propagate = False
-logger.addHandler(ch)
-logger.addHandler(fh)
 
 from weatherforecast import views
