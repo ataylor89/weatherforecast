e4312e913aa014f370cd38a66ae2e1ecd93de81f
Andrew Taylor <rolyata47@gmail.com>
2023-04-11 15:34:17 -0500

    adding detailed forecast

--- weatherforecast/static/js/weather.js
+++ weatherforecast/static/js/weather.js
@@ -76,6 +76,9 @@ $(document).ready(function() {
                 td7.html(period['shortForecast']);
                 tr.append(td7);
                 $('#forecast').append(tr);
+                let td8 = $('<td/>');
+                td8.html(period['detailedForecast']);
+                tr.append(td8);
             }
             $('#forecast').show();
         });
